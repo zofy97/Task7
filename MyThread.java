0769e89b0503a1373f6978ae5a798333277e5941
Zofy <32739550+zofy97@users.noreply.github.com>
2017-12-13 13:38:58 +0100

    Add files via upload

--- MyThread.java
+++ MyThread.java
@@ -11,6 +11,7 @@ package task7_1;
  * @author FIni
  */
 class MyThread extends Thread implements Runnable {
+	
     public void run()   {
         System.out.println("Begin run");
         for(int i = 0; i < 5; i++)  {
@@ -25,8 +26,12 @@ class MyThread extends Thread implements Runnable {
         }
     }
     public static void main(String[] args)  {
+    	
         MyThread t = new MyThread();
         t.start();
+        
+        for (int i = 0; i < 14850; i++)
+        	;
         
         MyThread s = new MyThread();
         try{
@@ -34,9 +39,10 @@ class MyThread extends Thread implements Runnable {
             s.start();
             } catch (InterruptedException e)    {
                 
+            } finally	{
+            	System.out.println(t.getName() + " Priorität: " + t.getPriority());
+            	System.out.println(s.getName() + " Priorität: " + s.getPriority());
             }
-        
-        System.out.println(t.getName() + " " + t.getPriority());
-        System.out.println(s.getName() + " " + s.getPriority());
+    	
     }
 }
--- ThreadsTest.java
+++ ThreadsTest.java
@@ -1,27 +1,22 @@
 package task7_2;
 
 import java.util.Random;
-import java.text.DecimalFormat;
 
 class Threads extends Thread implements Runnable   {
+	Konto k = new Konto(0.0);
     public void run()   {
-        int min1 = 2;
-        int max1 = 25;
         double min = 1;
         double max = 100;
         Random r = new Random();
         int count = 0;
-        String s;
-        double x;
-        Konto k = new Konto(0.0);
+        
         int randomValue1 = r.nextInt(10) + 2;
         for(int i = 0; i < randomValue1; i++)  {
             try{
                 double randomValue2 = min + (max - min) * r.nextDouble();
-                s = roundOffTo2DecPlaces(randomValue2);
-                x = Double.parseDouble(s.replace(",", "."));
                 Thread.sleep(1000);
-                k.einzahlen(x);
+                
+                k.einzahlen(randomValue2);
                 count++;
                 System.out.printf("%.2f", k.getKontostand());
                 System.out.println(" Euro");
@@ -29,16 +24,11 @@ class Threads extends Thread implements Runnable   {
                 interrupt();
             } 
         }
-        System.out.println("Es wurden insgesamt " + count + " Additionen von durchgeführt");
-    }
-    
-    String roundOffTo2DecPlaces(double val) {
-        return String.format("%.2f", val);
+        System.out.println("Es wurden insgesamt " + count + " Additionen von durchgeführt.");
     }
 }
 public class ThreadsTest {    
     public static void main(String[] args)  {
-        
         System.out.println("Begin\n");
         Threads t = new Threads();
         t.start();
